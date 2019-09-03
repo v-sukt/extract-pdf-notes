a04e52831ba60dfb1139f957c25614a2ca11059a
v-sukt <v-sukt@users.noreply.github.com>
2019-09-03 12:35:22 +0530

    local-code

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# container for easy use
+FROM ubuntu:18.04
+
+ADD extract_pdf_notes.py /usr/bin/extract_pdf_notes
+RUN apt update && \
+    apt install --quiet -y --no-install-recommends python3-poppler-qt5  && \
+    apt-get clean && \
+    chmod a+x /usr/bin/extract_pdf_notes && \
+    rm -rf \
+        /var/lib/apt/lists/* \
+        /tmp/* \
+        /var/tmp/* \
+        /usr/share/man \
+        /usr/share/doc \
+        /usr/share/doc-base && \
+    locale-gen en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US:en
+ENV LC_ALL en_US.UTF-8
+USER nobody
+WORKDIR /notes
+ENTRYPOINT ["/usr/bin/extract_pdf_notes"]
---
+++ extract_pdf_notes.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import popplerqt5
+import sys
+import PyQt5
+
+resolution = 150
+
+def main():
+
+    doc = popplerqt5.Poppler.Document.load(sys.argv[1])
+    print(sys.argv[1])
+    total_annotations = 0
+
+    for i in range(doc.numPages()):
+        #print("========= PAGE {} =========".format(i+1))
+        page = doc.page(i)
+        annotations = page.annotations()
+        (pwidth, pheight) = (page.pageSize().width(), page.pageSize().height())
+        count = 0
+
+        if len(annotations) > 0:
+
+            for annotation in annotations:
+
+                if isinstance(annotation, popplerqt5.Poppler.Annotation):
+                    total_annotations += 1
+                    
+                    if (isinstance(annotation, popplerqt5.Poppler.HighlightAnnotation)):
+                        quads = annotation.highlightQuads()
+                        txt = ""
+                        for quad in quads:
+                            rect = (quad.points[0].x() * pwidth,
+                                    quad.points[0].y() * pheight,
+                                    quad.points[2].x() * pwidth,
+                                    quad.points[2].y() * pheight)
+                            bdy = PyQt5.QtCore.QRectF()
+                            bdy.setCoords(*rect)
+                            txt = txt + str(page.text(bdy)) + ' '
+
+                        #print("========= ANNOTATION =========")
+                        sys.stdout(txt.encode('utf-8').decode('utf-8'))
+                        if annotation.contents(): 
+                        	sys.stdout("\t - {}".format(annotation.contents()))
+                        
+                    if isinstance(annotation, popplerqt5.Poppler.GeomAnnotation):
+                        count += 1
+                        bounds = annotation.boundary()
+
+                        # default we have height/width as per 72p rendering so converting to different resolution
+                        (width, height) = (pwidth*resolution/72, pheight*resolution/72)
+
+                        bdy = PyQt5.QtCore.QRectF(
+                            bounds.left()*width, 
+                            bounds.top()*height, 
+                            bounds.width()*width, 
+                            bounds.height()*height
+                        )
+                        
+                        page.renderToImage(resolution, resolution, bdy.left(), bdy.top(), bdy.width(), bdy.height()).save("page{}_image{}.png".format(i, count))
+                        print("page{}_image{}.png".format(i, count))
+                        if annotation.contents(): 
+                        	print(annotation.contents())
+                        
+                    if isinstance(annotation, popplerqt5.Poppler.TextAnnotation):
+                        if annotation.contents(): 
+                        	print(annotation.contents())
+
+    if total_annotations > 0:
+        pass
+    else:
+        print ("no annotations found")
+
+if __name__ == "__main__":
+    main()
